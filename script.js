c81b99b0bdfbe8d91ddf42b99902d7072eae1525
Dharm P <dharmpat@outlook.com>
2025-07-27 00:17:56 -0400

    added more changes to about section html, css, and js

--- script.js
+++ script.js
@@ -7,3 +7,34 @@ links.forEach(link => {
     link.classList.add('active');
   });
 });
+
+// for typing effect
+const roles = ["Dharm!", "A Student!", "An Engineer!"];
+let currentIndex = 0;
+let charIndex = 0;
+const typingText = document.getElementById("typing-text");
+
+function type() {
+  if (charIndex < roles[currentIndex].length) {
+    typingText.textContent += roles[currentIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, 100);
+  } else {
+    setTimeout(erase, 1500);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    typingText.textContent = roles[currentIndex].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(erase, 100);
+  } else {
+    currentIndex = (currentIndex + 1) % roles.length;
+    setTimeout(type, 500);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  type();
+});
