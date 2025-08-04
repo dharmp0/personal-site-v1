298a7164c5f1f6e4c4d81747e6d90901439df7c4
Dharm P <dharmpat@outlook.com>
2025-08-04 17:09:58 -0400

    Created work section into a timeline + general colouring changes

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ links.forEach(link => {
 });
 
 // for typing effect
-const roles = ["Dharm!", "a Student!", "an Engineer!"];
+const roles = ["Dharm!", "an Engineer!", "a Student!"];
 let currentIndex = 0;
 let charIndex = 0;
 const typingText = document.getElementById("typing-text");
